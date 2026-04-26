63bc7f87188f3b22740480c3bfbd9ae2a46d2b5d
atelier-yuki78 <mar2480.b@gmail.com>
2026-04-26 20:54:58 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,24 @@
+// worker.js
 importScripts('https://unpkg.com/swisseph@2.10.8-1/dist/swisseph.min.js');
 
 const PLANETS = [
-    swisseph.SE_SUN, swisseph.SE_MOON, swisseph.SE_MERCURY, swisseph.SE_VENUS,
-    swisseph.SE_MARS, swisseph.SE_JUPITER, swisseph.SE_SATURN, swisseph.SE_URANUS,
-    swisseph.SE_NEPTUNE, swisseph.SE_PLUTO, swisseph.SE_TRUE_NODE, swisseph.SE_CHIRON
+    swisseph.SE_SUN,       // 0: Q
+    swisseph.SE_MOON,      // 1: R
+    swisseph.SE_MERCURY,   // 2: S
+    swisseph.SE_VENUS,     // 3: T
+    swisseph.SE_MARS,      // 4: U
+    swisseph.SE_JUPITER,   // 5: V
+    swisseph.SE_SATURN,    // 6: W
+    swisseph.SE_URANUS,    // 7: X
+    swisseph.SE_NEPTUNE,   // 8: Y
+    swisseph.SE_PLUTO,     // 9: Z
+    swisseph.SE_TRUE_NODE, // 10: k (ドラゴンヘッド)
+    swisseph.SE_CHIRON     // 11: q (キロン)
 ];
+
+// 天文暦データの取得先を外部サーバーに設定
+// これにより、計算時に必要な .se1 ファイルが自動的にロードされます
+swisseph.swe_set_ephe_path('https://unpkg.com/swisseph@2.10.8-1/ephe/');
 
 self.onmessage = function(e) {
     const { id, date, time, isUnknown } = e.data;
@@ -13,7 +27,7 @@ self.onmessage = function(e) {
     const [year, month, day] = date.split('-').map(Number);
     const [hour, min] = time.split(':').map(Number);
     
-    // UTC+9 (日本時間) の調整
+    // 日本標準時(UTC+9)を考慮した調整
     const ut = (isUnknown ? 12 : hour) + (min / 60) - 9;
     
     swisseph.swe_julday(year, month, day, ut, swisseph.SE_GREG_CAL, (jd) => {
@@ -21,9 +35,15 @@ self.onmessage = function(e) {
         let completed = 0;
 
         PLANETS.forEach((p, idx) => {
+            // SEFLG_SPEED フラグで高精度計算を指定
             swisseph.swe_calc_ut(jd, p, swisseph.SEFLG_SPEED, (res) => {
+                if (res.error) {
+                    console.error(`Error calculating planet ${p}:`, res.error);
+                }
+                
                 planetsData[idx] = res.longitude;
                 completed++;
+
                 if (completed === PLANETS.length) {
                     self.postMessage({ id, planets: planetsData });
                 }
